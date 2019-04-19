7b86db25dcb5668a462c46cf583f662893c71cd2
Aaron Newton <git@clientcide.com>
2019-04-19 11:09:50 -0700

    removing iframe support

--- zendesk.js
+++ zendesk.js
@@ -391,7 +391,26 @@ window.Thanx = {
       this.openAdminIframe(event.target.href);
     },
 
+    openWindow: function(url){
+      var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+      var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+      var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+      var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+      var systemZoom = width / window.screen.availWidth;
+      var left = (width - w) / 2 / systemZoom + dualScreenLeft
+      var top = (height - h) / 2 / systemZoom + dualScreenTop
+      var newWindow = window.open(url, 'thanx-admin', 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+
+      // Puts focus on the newWindow
+      if (window.focus) newWindow.focus();
+    },
+
     openAdminIframe: function(url, force){
+      this.openWindow(url);
+      return
+
       if (!this.adminBackdrop && !this.adminModal) this.buildIframe();
 
       this.adminBackdrop.addClass('in').removeClass('hide');
